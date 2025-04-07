05e38772e4f4dc83534d7790d97b449457691de5
ian Kwon <sunghwan.ian.kwon@gmail.com>
2025-04-07 22:35:39 +0900

    style: 메인 페이지 디자인 템플릿 추가가

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,30 @@
 <script setup>
-
+const selectedSort = ref("최신순");
+const sortOptions = ["최신순", "인기순", "좋아요순"];
 </script>
 
 <template>
-  <div>
-    <h1>Home 페이지입니다!</h1>
-    <p>이건 Nuxt의 자동 라우팅 예제입니다.</p>
-  </div>
+  <v-container>
+    <LogoSection class="mx-auto" />
+    <Searchbar />
+    <v-row>
+      <v-col class="mt-5 ml-1"><h2>메인</h2></v-col>
+      <v-col>
+        <v-section>
+          <div class="mt-5 d-flex justify-end align-center">
+            <v-select
+              v-model="selectedSort"
+              :items="sortOptions"
+              density="compact"
+              variant="outlined"
+              hide-details
+              placeholder="정렬"
+            />
+          </div>
+        </v-section>
+      </v-col>
+    </v-row>
+
+    <CardList />
+  </v-container>
 </template>
-
-
-
