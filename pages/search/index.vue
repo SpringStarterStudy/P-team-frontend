2ff4f1fafdf5e83ca374e90c1cbd8e2b5a6c29ea
ian Kwon <sunghwan.ian.kwon@gmail.com>
2025-04-07 22:37:37 +0900

    style: 지도 검색 페이지 템플릿 추가가

---
+++ pages/search/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+   <LogoSection/>
+   <Searchbar/>
+  </v-container>
+      <div id="map" class="w-full h-100"></div>
+</template>
+
+<script setup>
+onMounted(() => {
+  if (typeof window.naver !== 'undefined' && window.naver.maps) {
+    const map = new window.naver.maps.Map('map', {
+      center: new window.naver.maps.LatLng(37.5665, 126.9780), // 서울시청 좌표
+      zoom: 15,
+    });
+  } else {
+    console.error('네이버 지도 로딩 실패: window.naver가 없습니다.');
+  }
+});
+</script>
+
+<style scoped>
+#map {
+  width: 100%;
+  height: 500px;
+}
+</style>
